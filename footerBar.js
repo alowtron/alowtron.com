bd32ddc3a95cb5f3a9b43020a7dd037808ac24b9
cfurlotte <alowtron@gmail.com>
2024-01-02 13:18:01 -0500

    Updated the footer with more info

--- footerBar.js
+++ footerBar.js
@@ -1,9 +1,12 @@
+//TODO: Link social medias
 document.write(
     `<footer>`,
         `<ul>`,
             `<h3>Social</h3>`,
-            `<li><a href="youtube.com">Youtube</a></li>`,
-
+            `<li><a href=\"http://www.github.com\">Github</a></li>`,
+            `<li><a href=\"http://www.youtube.com\">Youtube</a></li>`,
+            `<li><a href=\"http://www.reddit.com\">Reddit</a></li>`,
+            `<li><a href=\"http://www.linkedin.com\">LinkedIn</a></li>`,
         `</ul>`,
     `</footer>`,
 )
