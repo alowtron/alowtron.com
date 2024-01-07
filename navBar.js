616934c2739c2105367b90fec44c20e0a1cc19c6
cfurlotte <alowtron@gmail.com>
2024-01-07 18:50:46 -0500

    made progress on my blog

--- navBar.js
+++ navBar.js
@@ -29,12 +29,12 @@ function navBarDisplay(page, subfolder = false) {
         "<nav>",
             `<a href=\"index.html\"> <img src=\"${upFolder}images/logo.png\" class=\"logo\" alt=\"Logo\" ></img> </a>`,
                 "<ul>",
-                    "<li>",
+
                         `<li ${indexClass}><a href=\"${upFolder}index.html\">Home</a></li>`,
                         `<li ${projectClass}><a href=\"${upFolder}projects.html\">Projects</a></li>`,
                         `<li ${aboutClass}><a href=\"${upFolder}about.html\">About</a></li>`,
                         `<li ${blogClass}><a href=\"${upFolder}blog.html\">Blog</a></li>`,
-                    "</li>",
+
                 "</ul>",
         "</nav>"
     )
