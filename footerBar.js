618523a55307825ea63b2463d99887f5ee7e7ed9
cfurlotte <alowtron@gmail.com>
2024-01-13 20:37:49 -0500

    Added in basic mobile support and worked on the footer some more

--- footerBar.js
+++ footerBar.js
@@ -1,10 +1,16 @@
 //TODO: Link social medias
 document.write(
     `<ul>`,
+        `<li>Temp</li>`,
+    `</ul>`,
+    `<ul>`,
+    `</ul>`,
+    `<ul>`,
         `<h3>Social</h3>`,
         `<li><a href=\"http://www.github.com\">Github</a></li>`,
         `<li><a href=\"http://www.youtube.com\">Youtube</a></li>`,
         `<li><a href=\"http://www.reddit.com\">Reddit</a></li>`,
         `<li><a href=\"http://www.linkedin.com\">LinkedIn</a></li>`,
-    `</ul>`
+    `</ul>`,
+    `</div>`
 )
