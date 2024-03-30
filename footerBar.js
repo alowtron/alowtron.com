a7c1ee70ae740fcb273658bf300a40394be49938
cfurlotte <alowtron@gmail.com>
2024-03-29 20:06:05 -0400

    updates to the content on my website

--- footerBar.js
+++ footerBar.js
@@ -1,16 +1,20 @@
 //TODO: Link social medias
 document.write(
-    `<ul>`,
-        `<li>Temp</li>`,
+    `<ul style="content-align: center;">`,
+        `<h3> Links </h3>`,
+        
+        
     `</ul>`,
     `<ul>`,
     `</ul>`,
-    `<ul>`,
+    `<ul style="content-align: center;">`,
+        `<div style="text-align: center;">`,
         `<h3>Social</h3>`,
-        `<li><a href=\"http://www.github.com\">Github</a></li>`,
-        `<li><a href=\"http://www.youtube.com\">Youtube</a></li>`,
-        `<li><a href=\"http://www.reddit.com\">Reddit</a></li>`,
-        `<li><a href=\"http://www.linkedin.com\">LinkedIn</a></li>`,
+            `<li><a href=\"http://www.github.com\">Github</a></li>`,
+            `<li><a href=\"http://www.youtube.com\">Youtube</a></li>`,
+            `<li><a href=\"http://www.reddit.com\">Reddit</a></li>`,
+            `<li><a href=\"http://www.linkedin.com\">LinkedIn</a></li>`,
+        `</div>`,
     `</ul>`,
     `</div>`
 )
