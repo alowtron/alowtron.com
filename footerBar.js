1a67936e11f72beaa31e9ab02e5c99292ef56923
alowtron <147556072+alowtron@users.noreply.github.com>
2024-10-14 14:50:26 -0400

    Add files via upload

--- footerBar.js
+++ footerBar.js
@@ -10,11 +10,14 @@ document.write(
     `<ul style="content-align: center;">`,
         `<div style="text-align: center;">`,
         `<h3>Social</h3>`,
-            `<li><a href=\"http://www.github.com\">Github</a></li>`,
-            `<li><a href=\"http://www.youtube.com\">Youtube</a></li>`,
-            `<li><a href=\"http://www.reddit.com\">Reddit</a></li>`,
-            `<li><a href=\"http://www.linkedin.com\">LinkedIn</a></li>`,
+            `<li><a href=\"http://www.github.com/alowtron\">Github</a></li>`,
+            
+            `<li><a href=\"http://www.linkedin.com/in/alowtron\">LinkedIn</a></li>`,
         `</div>`,
     `</ul>`,
     `</div>`
 )
+
+// social links for future
+// `<li><a href=\"http://www.youtube.com\">Youtube</a></li>`,
+// `<li><a href=\"http://www.reddit.com\">Reddit</a></li>`,
